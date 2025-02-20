444e1fe35f7320db093454935c2d85cec4f3945b
JulianCastillo14 <juliancast140502@gmail.com>
2025-02-20 11:14:44 -0500

    Creando modelos de las entidades a utilizar y repositorio para la escritura de mensajes

---
+++ domian/src/main/java/com/module/domian/entity/Header.java
@@ -0,0 +1,62 @@
+package com.module.domian.entity;
+
+import java.time.Instant;
+
+public class Header {
+    private String userUUID;
+
+    private String deviceId;
+
+    private Instant timeStamp;
+
+    private String location;
+
+    public Header(String userUUID, String deviceId, String location){
+        this.userUUID = userUUID;
+        this.deviceId = deviceId;
+        timeStamp = Instant.now();
+        this.location = location;
+    }
+
+    public String getUserUUID() {
+        return userUUID;
+    }
+
+    public void setUserUUID(String userUUID) {
+        this.userUUID = userUUID;
+    }
+
+    public String getDeviceId() {
+        return deviceId;
+    }
+
+    public void setDeviceId(String deviceId) {
+        this.deviceId = deviceId;
+    }
+
+    public Instant getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(Instant timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    @Override
+    public String toString() {
+        return "Header{" +
+                "userUUID='" + userUUID + '\'' +
+                ", deviceId='" + deviceId + '\'' +
+                ", timeStamp=" + timeStamp +
+                ", location='" + location + '\'' +
+                '}';
+    }
+}
---
+++ domian/src/main/java/com/module/domian/repository/MessageRepository.java
@@ -0,0 +1,7 @@
+package com.module.domian.repository;
+
+import com.module.domian.entity.Message;
+
+public interface MessageRepository {
+    Message write(Message message);
+}
